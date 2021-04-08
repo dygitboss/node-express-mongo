640e9b6f2a996f8271a358b16a02ef9e33e235f6
dygitboss <office@dygit.io>
2021-04-08 15:56:10 +0300

    Packages upgrade;

--- src/models/index.js
+++ src/models/index.js
@@ -18,7 +18,8 @@ fs
   .readdirSync(__dirname)
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
+    // eslint-disable-next-line global-require,import/no-dynamic-require
+    const model = require(path.join(__dirname, file)).default(sequelize, Sequelize.DataTypes);
     models[model.name] = model;
   });
 
